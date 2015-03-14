66b20e4c9f59756ed0347dbf89fa8dd42623703b
Boundary SAP <boundary.sap.dev@gmail.com>
2015-03-14 19:37:00 +0000

    Cliclo infinito
    Signed-off-by: Boundary SAP <boundary.sap.dev@gmail.com>

--- index.java
+++ index.java
@@ -2,8 +2,25 @@
 public class index {
 
 	public static void main(String[] args) {
-		
-		System.out.println("WIN-M1VS2S14H35\tCPU_CORE_FD021\t5");
+		long poll = 1000;
+		if (args.length>0) {
+			System.err.println("Polling time :"+args[0]);
+			poll = Long.parseLong(args[0]); 
+		}
+
+		while (true ) {
+
+
+			try {
+				Thread.sleep(poll);
+				System.out.println("WIN-M1VS2S14H35\tCPU_CORE_FD021\t5");
+			} catch (InterruptedException e) {
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+
+		}
+
 
 	}
 
