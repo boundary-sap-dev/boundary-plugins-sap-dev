9aecbd130fcfcd3c0cd77fce7f361a924a40b641
Boundary SAP <boundary.sap.dev@gmail.com>
2015-03-14 17:37:35 +0000

    cambio de nombre

--- index.java
+++ index.java
@@ -3,7 +3,7 @@ public class index {
 
 	public static void main(String[] args) {
 		
-		System.out.println("WIN-M1VS2S14H35\tCPU_CORE_FD01\t5");
+		System.out.println("WIN-M1VS2S14H35\tCPU_CORE_FD021\t5");
 
 	}
 
